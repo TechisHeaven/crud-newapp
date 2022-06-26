893bb3943fe47d0c336c27118b9ee47fd054d2de
TechisHeaven <vermajihimanshu2004@gmail.com>
2022-06-26 16:16:24 +0530

    commit

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.set("view engine", "ejs")
 
 // server favicon files 
 
-app.use(favicon(__dirname + '/favicon.ico'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 
 //load assests ----------------------------------
