3fc3b3dde95bdf77a90f46e4224605641efceaba
TechisHeaven <vermajihimanshu2004@gmail.com>
2022-06-29 14:43:52 +0530

    co

--- server/services/render.js
+++ server/services/render.js
@@ -5,30 +5,28 @@ const PORT = process.env.PORT || 8080
 
 
 
-exports.homeRoutes = (req, res)=>{
-    
+exports.homeRoutes = (req, res) => {
+
     // Make a get request to /api/users 
     axios.get(`http://localhost:${PORT}/api/users`)
-        .then(function(response){
-            res.render('index',{users : response.data})
+        .then(function (response) {
+            res.render('index', { users: response.data })
         })
-        .catch(err=>{
+        .catch(err => {
             res.send(err)
         })
-    
+
 }
 
-exports.notfoundpage = (req, res)=>{
+exports.notfoundpage = (req, res) => {
+
     
-    // Make a get request to /api/users 
-    axios.get(`http://localhost:${PORT}`)
-        .then(function(response){
-            res.render('404')
-        })
-        .catch(err=>{
+
+    res.render('404')
+        .catch(err => {
             res.send(err)
         })
-    
+
 }
 
 
@@ -36,18 +34,18 @@ exports.notfoundpage = (req, res)=>{
 
 
 
-exports.add_user = (req, res)=>{
+exports.add_user = (req, res) => {
     res.render('add_user')
 
 }
 
-exports.update_user = (req, res)=>{
+exports.update_user = (req, res) => {
     //render update user
-    axios.get(`http://localhost:${PORT}/api/users`,{params:{id:req.query.id}})
-        .then(function(userdata){
-            res.render("update_user",{user : userdata.data})
+    axios.get(`http://localhost:${PORT}/api/users`, { params: { id: req.query.id } })
+        .then(function (userdata) {
+            res.render("update_user", { user: userdata.data })
         })
-        .catch(err=>{
+        .catch(err => {
             res.send(err)
         })
 }
